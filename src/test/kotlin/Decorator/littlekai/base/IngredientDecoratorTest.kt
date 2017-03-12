ace77db4daf9d7d890841538f667d5fc38e905f8
Antonio López Marín <tonilopezmr@gmail.com>
2017-03-12 20:58:21 +0100

    Added test example

---
+++ src/test/kotlin/Decorator/littlekai/base/IngredientDecoratorTest.kt
@@ -0,0 +1,15 @@
+package Decorator.littlekai.base
+
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert.assertThat
+import org.junit.Test
+
+class IngredientDecoratorTest {
+
+  @Test
+  fun `example of junit test`() {
+    val string = "Hola Sergio :D"
+    assertThat(string, `is`("Hola Sergio :D"))
+  }
+
+}
