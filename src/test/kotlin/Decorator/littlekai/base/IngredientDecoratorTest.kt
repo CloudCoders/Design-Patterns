2e386ad0df0c3bc4e8aad3ded37a6ef53461c1fb
Antonio López Marín <tonilopezmr@gmail.com>
2017-03-14 01:20:14 +0100

    Moved test corresponding with noodle test @franlo

--- src/test/kotlin/Decorator/littlekai/base/IngredientDecoratorTest.kt
+++ src/test/kotlin/Decorator/littlekai/base/IngredientDecoratorTest.kt
@@ -16,7 +16,6 @@ class IngredientDecoratorTest {
   fun `check if decorated element is still Noodles`() =
     assertTrue(Pork(UdonNoodles()) is Noodles) //:+1:
 
-
   @Test
   fun `Ingredient Decorator should return ingredient cost plus base noodle cost`() =
     assertThat(Pork(UdonNoodles()).calculateCost(), `is`(8.00))
@@ -28,23 +27,5 @@ class IngredientDecoratorTest {
   @Test
   fun `price of ingredient is calculated correctly`() =
     assertThat(Tuna(Pork(UdonNoodles())).calculateCost(), `is`(10.75))
-
-  @Test
-  fun `check correctness of wheat noodles's cost`(){
-    val wheatNoodles = WheatNoodles()
-    assertThat(wheatNoodles.calculateCost(), `is`(3.50))
-  }
-
-  @Test
-  fun `check correctness of egg noodles's cost`(){
-    val eggNoodles = EggNoodles()
-    assertThat(eggNoodles.calculateCost(), `is`(3.75))
-  }
-
-  @Test
-  fun `check correctness of udon noodles's cost`(){
-    val udonNoodles = UdonNoodles()
-    assertThat(udonNoodles.calculateCost(), `is`(4.00))
-  }
 
 }
