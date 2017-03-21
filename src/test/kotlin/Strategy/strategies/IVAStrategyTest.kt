5260503ece15111fc702498ca137dd9200b2efc9
Fran López <fcolopaguado@gmail.com>
2017-03-21 10:21:35 +0100

    Fixed hardcoded expected result.

--- src/test/kotlin/Strategy/strategies/IVAStrategyTest.kt
+++ src/test/kotlin/Strategy/strategies/IVAStrategyTest.kt
@@ -7,16 +7,16 @@ class IVAStrategyTest{
 
   @Test
   fun `check that general iva is applied correctly`(){
-    assertThat(GeneralStrategy(2.0), CoreMatchers.`is`(2+2*0.21))
+    assertThat(GeneralStrategy(2.0), CoreMatchers.`is`(2.42))
   }
 
   @Test
   fun `check that reduced iva is applied correctly`(){
-    assertThat(ReducedStrategy(2.0), CoreMatchers.`is`(2+2*0.1))
+    assertThat(ReducedStrategy(2.0), CoreMatchers.`is`(2.02))
   }
 
   @Test
   fun `check that super-reduced iva is applied correctly`(){
-    assertThat(SuperReducedStrategy(2.0), CoreMatchers.`is`(2+2*0.04))
+    assertThat(SuperReducedStrategy(2.0), CoreMatchers.`is`(2.08))
   }
 }
