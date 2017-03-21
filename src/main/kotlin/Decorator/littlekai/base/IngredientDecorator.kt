917739544c27c2cf20196d40dd2e657300093455
Sergio Trujillo Martínez <sergiotrujillomartinez@gmail.com>
2017-03-21 20:49:42 +0100

    Remove unnecessary override of function calculateTotalCost() in IngredientDecorator

--- src/main/kotlin/Decorator/littlekai/base/IngredientDecorator.kt
+++ src/main/kotlin/Decorator/littlekai/base/IngredientDecorator.kt
@@ -1,5 +1,3 @@
 package Decorator.littlekai.base
 
-abstract class IngredientDecorator (private val noodles: Noodles): Noodles by noodles {
-  override fun calculateTotalCost(): Double = noodles.calculateTotalCost()
-}
+abstract class IngredientDecorator (private val noodles: Noodles): Noodles by noodles
