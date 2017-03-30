e8a13ebf42b898232705ceb76df6ebb1f297b636
Antonio López Marín <tonilopezmr@gmail.com>
2017-03-31 01:32:46 +0200

    Teaching @Cotel example of tests that he wanted to do

--- src/test/kotlin/oop/Composite/CompositeTest.kt
+++ src/test/kotlin/oop/Composite/CompositeTest.kt
@@ -1,59 +1,64 @@
 package oop.Composite
 
-import oop.Composite.base.ChineseCooker
 import oop.Composite.base.Cooker
 import oop.Composite.base.ItalianCooker
+import oop.Composite.base.Kitchen
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 
 class CompositeTest {
 
+  /**
+   * Example of "spies" without using your MockKitchen because don't need that
+   */
   @Test
   fun `composite should allow to add more childrens`() {
-    val kitchen = MockKitchen()
-    assertThat(kitchen.cookers.size, `is`(0))
+    val cookers = mutableListOf<Cooker>()
+    val kitchen = Kitchen(cookers)
+    assertThat(cookers.size, `is`(0))
 
     val italian = ItalianCooker()
     kitchen.add(italian)
-    assertThat(kitchen.cookers.size, `is`(1))
+    assertThat(cookers.size, `is`(1))
   }
 
   @Test
   fun `composite should allow to remove childrens`() {
-    val kitchen = MockKitchen()
+    val cookers = mutableListOf<Cooker>()
+    val kitchen = Kitchen(cookers)
     val italian = ItalianCooker()
     kitchen.add(italian)
-    assertThat(kitchen.cookers.size, `is`(1))
+    assertThat(cookers.size, `is`(1))
 
     kitchen.remove(italian)
-    assertThat(kitchen.cookers.size, `is`(0))
+    assertThat(cookers.size, `is`(0))
   }
 
+  /**
+   * HERE YOU WANT TO KNOW IF ALL COOKERS ARE CALLED :+1:
+   */
   @Test
   fun `times cooked should be equal to cookers length after cooking with all childrens of composite`() {
-    val kitchen = MockKitchen()
-    val italian = ItalianCooker()
-    val chinese = ChineseCooker()
+    val cookers = mutableListOf<Cooker>()
+    val kitchen = Kitchen(cookers)
+    val italian = MockCooker()
+    val chinese = MockCooker()
     kitchen.add(italian)
     kitchen.add(chinese)
 
     kitchen.cook()
-    assertThat(kitchen.timesCooked, `is`(kitchen.cookers.size))
+    assertThat(italian.times + italian.times, `is`(cookers.size))
   }
 
 }
 
-class MockKitchen(var cookers: MutableList<Cooker> = mutableListOf()) : Cooker {
-  var timesCooked : Int = 0
+class MockCooker : Cooker {
+
+  var times = 0
 
   override fun cook() {
-    timesCooked = cookers.map(Cooker::cook).size
+    times++
   }
 
-  fun add(cooker: Cooker) = cookers.add(cooker)
-
-  fun remove(cooker: Cooker) {
-    cookers.remove(cooker)
-  }
 }
