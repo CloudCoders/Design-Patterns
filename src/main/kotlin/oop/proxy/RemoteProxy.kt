d96f2dfe1790511811159b29918887a518291c0f
Antonio López Marín <tonilopezmr@gmail.com>
2017-04-26 19:13:11 +0200

    Added remote proxy example

---
+++ src/main/kotlin/oop/proxy/RemoteProxy.kt
@@ -0,0 +1,54 @@
+package oop.proxy
+
+interface Message {
+  fun writeInChannel(message: String, channel: String)
+}
+
+class MessageRemoteProxy(val ouputStream: SimulateOutputStream) : Message {
+
+  private val addHeadTCP = ":TCPInformationLOL:"
+  private val tailTCP = ":TCPendlol:"
+
+  override fun writeInChannel(message: String, channel: String) {
+    ouputStream.write(addHeadTCP + message + ":" + channel + tailTCP)
+    ouputStream.close()
+  }
+}
+
+class MessageServerObject(val serverDomain: ServerDomain = ServerDomain()) : Message {
+
+  override fun writeInChannel(message: String, channel: String) {
+    serverDomain.channels.forEach {
+      if (it == channel){
+        println("Message in $channel: $message")
+      }
+    }
+  }
+
+}
+
+class ServerDomain(val channels: MutableList<String> = mutableListOf())
+
+class SimulateOutputStream {
+
+  fun write(string: String) {
+    println("write message on network: $string")
+
+    //when you sends the information another remote API,
+    // grab this information and transform to remote object in his machine
+
+    //IN MY SERVER
+
+    var (message, channel) = string
+      .removePrefix(":TCPInformationLOL:")
+      .removeSuffix(":TCPendlol:")
+      .split(":")
+
+    var messageServerObject = MessageServerObject()
+    messageServerObject.writeInChannel(message, channel)
+
+    //MY SERVER LEFT
+  }
+
+  fun close() {}
+}
